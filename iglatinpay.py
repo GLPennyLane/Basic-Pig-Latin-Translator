98d3d79c58ae41f7e94dfebf7e6541b7675143c2
Wasted <105006648+GLPennyLane@users.noreply.github.com>
2022-10-24 15:35:35 -0700

    Create iglatinpay.py

---
+++ iglatinpay.py
@@ -0,0 +1,9 @@
+while True:
+  print("please enter the stuff you want to translate or q to quit!")
+  text = input()
+  if text == "q":
+    break
+  letter = text[0]
+  text = text[1:]
+  letter = letter + 'ay'
+  print(text + letter)
